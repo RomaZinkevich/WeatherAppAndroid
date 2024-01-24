ca1edc4e39674aefb9c85bd3ce1431d96336edad
RomaZinkevich <zra1903@mail.ru>
2024-01-24 16:48:42 +0200

    design done

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppUI()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WeatherAppUI() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(0.dp).background(Color.Cyan),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Tampere",
+            fontSize = 60.sp,
+            fontFamily = FontFamily.Monospace,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth())
+        Row( modifier = Modifier.padding(16.dp) ) {
+            Image(
+                painter = painterResource( id = R.drawable.sunny ),
+                contentDescription = "Sunny icon",
+                modifier = Modifier.size(100.dp)
+            )
+            Spacer(modifier = Modifier.width(100.dp))
+            Text(text = "28C", fontSize = 35.sp, textAlign = TextAlign.Center,
+                modifier = Modifier
+                .align(Alignment.CenterVertically))
+        }
+        Text(text = "Sunny", fontSize = 30.sp)
+
+        Spacer(modifier = Modifier.weight(1f))
+        Button(onClick = { }) {
+            Text(text = "Refresh weather data")
+        }
+    }
+
+}
